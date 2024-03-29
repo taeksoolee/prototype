13c21d13baadf7a8bf22e6fedb3d43c89881b21a
leetaeksoo <leets1490@gmail.com>
2024-03-29 12:02:11 +0900

    Feat: Object[Symbol.iterator], Object.noop, Object.forEach

--- index.js
+++ index.js
@@ -1,9 +1,24 @@
-String.prototype.removed = function(word=' ') {
-  return this.toString().replace(new RegExp(word, 'g'),'');
-}
-
 function unsafe__extend () {
+  Object.noop = function() {
+    // noop
+  }
+  
+  String.prototype.removed = function(word=' ') {
+    return this.toString().replace(new RegExp(word, 'g'),'');
+  }
+
   Object.prototype[Symbol.iterator] = function* () {
-    for (const i in this) { yield [i, this[i]] }
+    for (const i of Object.keys(this)) { yield [i, this[i]] }
+  }
+
+  /**
+   * 
+   * @param {(key, value) => void} callback 
+   */
+  Object.prototype.forEach = function(callback=Object.noop) {
+    for (const [key, value] of Object(this)) {
+      // console.log(key, value, this);
+      callback(value, key, this);
+    }
   }
 }
